0e9c60cc0a4fb07706da237e1982f82b78395b0f
JohnSteimle <steimle.7@wright.edu>
2023-12-01 12:51:13 -0500

    website commit

---
+++ website/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple Website</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Welcome to Our Simple Website</h1>
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <h2>Featured Content</h2>
+        <p>Here is a featured content for this page. Feel free to replace this with your own content.</p>
+    </main>
+
+    <footer>
+        <p>&copy; 2022 Our Simple Website. All rights reserved.</p>
+    </footer>
+</body>
+</html>
